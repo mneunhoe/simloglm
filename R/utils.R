698b4ed9ac71a680888fd05d06f7395ca3cef1a7
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2022-03-10 15:55:33 -0500

    interaction example

--- R/utils.R
+++ R/utils.R
@@ -33,3 +33,59 @@ sim_logy <- function(nsim, E_log_Y, sigma, exponentiate = TRUE){
   if (exponentiate == TRUE){return(exp(logY_sim))}
   if (exponentiate == FALSE){return(logY_sim)}
 }
+
+
+setup_pop <-
+  function(n,
+           ff,
+           type = "num",
+           coefs = 1,
+           zero_centered = TRUE,
+           noise_sd = 1) {
+    variables <- all.vars(as.formula(ff))
+    variable_list <- list()
+
+    if (length(type) == length(variables) - 1) {
+      type_vec <- type
+    } else {
+      type_vec <- rep(type[1], length(variables) - 1)
+    }
+
+    if (zero_centered == T) {
+      for (variable in variables[2:length(variables)]) {
+        variable_list[[paste0(variable)]] <-
+          if (type_vec[variable == variables[2:length(variables)]] == "bin") {
+            rbinom(n, size = 1, prob = 0.5)
+          } else {
+            rnorm(n, 0, 1)
+          }
+      }
+    } else {
+      for (variable in variables[2:length(variables)]) {
+        variable_list[[paste0(variable)]] <-
+          if (type_vec[variable == variables[2:length(variables)]] == "bin") {
+            rbinom(n, size = 1, prob = 0.5)
+          } else {
+            rnorm(n, 7, 1.5)
+          }
+      }
+    }
+    tmp_df <- do.call(data.frame, variable_list)
+    tmp_ff <- as.formula(sub(".*~", "~", ff))
+    mf <- model.frame(tmp_ff, tmp_df)
+    mm <- model.matrix(tmp_ff, data = mf)
+    # If the number of provided coefficients is not the same number as the number
+    # of actual coefficients we take the first value of the vector and set all
+    # coefficients to that value.
+    if (length(coefs) == ncol(mm)) {
+      coef_vec <- coefs
+    } else {
+      coef_vec <- rep(coefs[1], ncol(mm))
+    }
+    # Create the dependent variable based on the model matrix and coefficients.
+    # Add some random noise.
+    variable_list[[paste0(variables[1])]] <-
+      mm %*% coef_vec + rnorm(n, 0, noise_sd)
+    df <- do.call(data.frame, variable_list)
+    return(df)
+  }
